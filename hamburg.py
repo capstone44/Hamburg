e5b60bb08adb8b9e7b42a88df1ba81423d1bdc18
Sean <sheath51@gmail.com>
2019-02-05 08:10:16 -0700

    Converted everything to a flask templating engine, now is sane and stable

--- hamburg.py
+++ hamburg.py
@@ -2,6 +2,10 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
+@app.route('/static/<path:path>')
+def send_js(path):
+        return send_from_directory('static', path)
+
 @app.route("/")
 def index():
     return render_template('templates/index.html')
