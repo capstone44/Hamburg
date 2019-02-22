bd0d3d151ff49d1345c137e24e097662b01dd153
Enoch Boekweg <etboekweg@gmail.com>
2019-02-22 09:54:26 -0700

    Updates and stuff

--- hamburg.py
+++ hamburg.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+import JSON
 app = Flask(__name__)
 
 @app.route('/static/<path:path>')
@@ -18,10 +19,26 @@ def quickstart():
 def plotmeasurement():
     return render_template('plotmeasurement.html')
 
+@app.route("/start")
+def start_test():
+    #Send command to Sean to start AUT rotation and measurement...
+    print("Starting rotation and AUT measurement")
+
 @app.route("/angle")
 def setrotation():
+    #Send command over socket to Sean's Code
     print("Set new rotation to: ")
     
 @app.route("/scratch")
 def scratch():
     return render_template('scratch.html')
+
+@app.route("/getdata")
+def serialize_plot_data():
+    data = {
+        type = "scatterpolar",
+        mode = "markers",
+        r   = [],
+        theta = []
+    }
+    return JSON.serialize(data)
